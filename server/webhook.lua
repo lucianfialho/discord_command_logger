2620d0622e956bb36b9c176c7e3d780ca6f4d2df
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-11-01 16:27:25 -0300

    -- passando informacoes do usuário

--- server/webhook.lua
+++ server/webhook.lua
@@ -10,31 +10,55 @@ function Webhook(webHookUrl, webHookImage)
     end
 
     self.send = function(playerId, rawCommand)
-        local messageObj = self.messageBuilder(playerId, rawCommand)
+        local user = self.getPlayerServerInfo()
+        local messageObj = self.messageBuilder(user, rawCommand)
         PerformHttpRequest(self.webHookUrl, function(err, text, header) print(err, text) end,
         'POST',
         json.encode(messageObj), {['Content-Type'] = 'application/json'}) 
     end
 
-    self.messageBuilder = function(playerId, rawCommand)
-        local playerName = GetPlayerName(source)
+    self.messageBuilder = function(user, rawCommand)
+        local description = self.createDescription(user)
     
         return {
             embeds = {
               {
-                title = playerName..' was execute a command',
-                description = '```'.. rawCommand ..'```',
+                title = user.name ..' was execute a command',
+                description = '```'.. rawCommand ..'```\n'.. description,
                 url = 'https://github.com/lucianfialhobp',
                 color = 3666853,
                 thumbnail = {
                   url = self.webHookImage
                 },
                 author = {
-                  name = GetPlayerName(source),
+                  name = user.name,
                 },
               },
             }
           }
+    end
+
+    self.getPlayerServerInfo = function ()
+      local user = {}
+      user.name = GetPlayerName(source)
+  
+      for k,v in pairs(GetPlayerIdentifiers(source)) do              
+        if string.sub(v, 1, string.len("steam:")) == "steam:" then
+          user.steamid = v
+        elseif string.sub(v, 1, string.len("license:")) == "license:" then
+          user.license = v
+        elseif string.sub(v, 1, string.len("ip:")) == "ip:" then
+          user.ip = v
+        elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
+          user.discord = v
+        end
+      end
+  
+      return user
+    end
+
+    self.createDescription = function(user)
+      return string.format("Steam: %s |\n License: %s |\n Ip: %s |\nt Discord: %s |", user.steamid, user.license, user.ip, user.discord)
     end
 
     return self
