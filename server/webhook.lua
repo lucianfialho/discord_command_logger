0e640fd6c2407bded0b4d6365995cb7714336036
Lucas Lima <lucas39624@gmail.com>
2020-11-01 18:19:00 -0300

    -- Adding more identifiers

--- server/webhook.lua
+++ server/webhook.lua
@@ -12,55 +12,92 @@ function Webhook(webHookUrl, webHookImage)
     self.send = function(playerId, rawCommand)
         local user = self.getPlayerServerInfo()
         local messageObj = self.messageBuilder(user, rawCommand)
-        PerformHttpRequest(self.webHookUrl, function(err, text, header) print(err, text) end,
-        'POST',
-        json.encode(messageObj), {['Content-Type'] = 'application/json'}) 
+        PerformHttpRequest(self.webHookUrl, function(err, text, header) print(err, text) end, 'POST', json.encode(messageObj), {
+            ['Content-Type'] = 'application/json'
+        })
     end
 
     self.messageBuilder = function(user, rawCommand)
-        local description = self.createDescription(user)
-    
+
         return {
             embeds = {
-              {
-                title = user.name ..' was execute a command',
-                description = '```'.. rawCommand ..'```\n'.. description,
-                url = 'https://github.com/lucianfialhobp',
-                color = 3666853,
-                thumbnail = {
-                  url = self.webHookImage
+                {
+                    title = user.name ..' was execute a command',
+                    description = '```'.. rawCommand ..'```\n',
+                    url = 'https://github.com/lucianfialhobp',
+                    color = 3666853,
+
+                    fields = {
+                        {
+                            name = 'Steam Hex:',
+                            value = user.steamhex,
+                            inline = true
+                        },
+                        {
+                            name = 'Rockstar License:',
+                            value = user.license,
+                            inline = true
+                        },
+                        {
+                            name = 'Xbox:',
+                            value = user.xbox,
+                            inline = true
+                        },
+                        {
+                            name = 'IP:',
+                            value = user.ip,
+                            inline = true
+                        },
+                        {
+                            name = 'Discord ID:',
+                            value = user.discord,
+                            inline = true
+                        },
+                        {
+                            name = 'Microsoft:',
+                            value = user.microsoft,
+                            inline = true
+                        }
+                    },
+
+                    thumbnail = {
+                        url = self.webHookImage
+                    },
+
+                    author = {
+                        name = user.name,
+                    },
                 },
-                author = {
-                  name = user.name,
-                },
-              },
             }
-          }
+        }
     end
 
     self.getPlayerServerInfo = function ()
-      local user = {}
-      user.name = GetPlayerName(source)
-  
-      for k,v in pairs(GetPlayerIdentifiers(source)) do              
-        if string.sub(v, 1, string.len("steam:")) == "steam:" then
-          user.steamid = v
-        elseif string.sub(v, 1, string.len("license:")) == "license:" then
-          user.license = v
-        elseif string.sub(v, 1, string.len("ip:")) == "ip:" then
-          user.ip = v
-        elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
-          user.discord = v
+        local user = {}
+        user.name = GetPlayerName(source)
+
+        for k,v in pairs(GetPlayerIdentifiers(source)) do              
+            if string.sub(v, 1, string.len("steam:")) == "steam:" then -- Steam Hex
+                user.steamhex = string.sub(v, 7)
+            elseif string.sub(v, 1, string.len("license:")) == "license:" then -- Rockstar License
+                user.license = string.sub(v, 9)
+            elseif string.sub(v, 1, string.len("xbl:")) == "xbl:" then -- Xbox
+                user.xbox = string.sub(v, 5)
+            elseif string.sub(v, 1, string.len("ip:")) == "ip:" then -- Ip
+                user.ip = string.sub(v, 4)
+            elseif string.sub(v, 1, string.len("discord:")) == "discord:" then -- Discord ID
+                user.discord = string.sub(v, 9)
+            elseif string.sub(v, 1, string.len("live:")) == "live:" then -- Microsoft
+                user.microsoft = string.sub(v, 6)
+            end
         end
-      end
-  
-      return user
+
+        return user
     end
 
     self.createDescription = function(user)
-      return string.format("Steam: %s |\n License: %s |\n Ip: %s |\nt Discord: %s |", user.steamid, user.license, user.ip, user.discord)
+        return string.format("Steam: %s |\n License: %s |\n Xbox: %s |\n Ip: %s |\n Discord: %s |\n Microsoft: %s |", user.steamhex, user.license, user.xbox, user.ip, user.discord, user.microsoft)
     end
 
     return self
-
 end
